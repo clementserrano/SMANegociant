c6c0bf031d77b213a51d413df3e0d7ae115b3e7a
Martial Tardy <tardy.m02@gmail.com>
2019-02-04 14:27:58 +0100

    2nd fournisseur with 2nd billet

--- src/Main.java
+++ src/Main.java
@@ -39,9 +39,31 @@ public class Main {
         fournisseur.getNegociants().add(negociant2);
         fournisseur.setName("Fournisseur 1");
 
+        Fournisseur fournisseur2 = new Fournisseur();
+
+        Billet billet2 = new Billet();
+        billet2.setLieuDepart(Lieu.PARIS);
+        billet2.setLieuArrivee(Lieu.MARSEILLE);
+
+        billet2.setDateDepart(Utils.datePlusDays(10));
+        billet2.setDateArrivee(Utils.datePlusDays(11));
+
+        fournisseur2.setValeurDepart(400.0);
+        fournisseur2.setPrixMin(100.0);
+        fournisseur2.setBillet(billet2);
+        fournisseur2.setDateVenteAuPlusTard(Utils.datePlusDays(9));
+
+        fournisseur2.getNegociants().add(negociant);
+        fournisseur2.getNegociants().add(negociant2);
+        fournisseur2.setName("Fournisseur 2");
+
         negociant.getFournisseurs().add(fournisseur);
+        negociant.getFournisseurs().add(fournisseur2);
+        negociant2.getFournisseurs().add(fournisseur);
+        negociant2.getFournisseurs().add(fournisseur2);
 
         new Thread(fournisseur).start();
+        new Thread(fournisseur2).start();
         new Thread(negociant).start();
         new Thread(negociant2).start();
     }
